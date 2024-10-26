b32512c4428a772bc5f121ecb5bbc4ff94dfa69b
ryan <39787691+parven-dev@users.noreply.github.com>
2024-10-26 20:53:06 +0530

    changed function name

--- Array/Lists/extend_list.py
+++ Array/Lists/extend_list.py
@@ -1,9 +1,9 @@
 
-def insert_element(arr : list, arr2):
+def extend_array: list, arr2):
     arr.extend(arr2)
     return arr
 
 
 arr = [22,21,42,43,53,32,51]
 arr2 = [33, 44,55,66,77,88]
-print(insert_element(arr, arr2))
+print(extend_array(arr, arr2))
