9c93418079946dbe247b354a72b058e0a49e3a50
ryan <39787691+parven-dev@users.noreply.github.com>
2024-11-04 15:20:31 +0530

    creating dictionary

---
+++ Dictionary/creating_dict/main.py
@@ -0,0 +1,18 @@
+
+
+# there are two ways
+
+#first 
+
+myDict = dict()
+print(type(myDict))
+
+# second
+myDict2 = {}
+print(type(myDict2))
+
+
+numToWord = {1: "one", 2:"two", 3: "three"}
+print(numToWord)
+
+print(numToWord[2])
