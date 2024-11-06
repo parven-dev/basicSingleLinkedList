5910098c748d056e5e6a3477ed64b28871e9d55c
ryan <39787691+parven-dev@users.noreply.github.com>
2024-11-06 15:28:38 +0530

    Delete Tuple/creating_tuple directory

--- Tuple/creating_tuple/main.py
+++
@@ -1,5 +0,0 @@
-my_tuples = ("a", "b", "c", "d")
-
-my_tuples1 = ("a",) # ("a") its not a tuple
-my_tuples2 = tuple("a") # this type of tuple declaration is not deal, avoid using 
-print(type(my_tuples2))
