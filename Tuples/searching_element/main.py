57aae9a4b215e7bccf1e4e99da74c4b86291f506
ryan <39787691+parven-dev@users.noreply.github.com>
2024-11-06 16:01:54 +0530

    searching elements

---
+++ Tuples/searching_element/main.py
@@ -0,0 +1,12 @@
+
+
+my_tuples = ("a", "b", "c", "d")
+searching_value = "d"
+def searching_element(tuples: tuple, ele):
+    for item in range(len(tuples)):
+        if tuples[item] == ele:
+            return tuples.index(tuples[item])
+
+
+
+print(searching_element(my_tuples, searching_value))
