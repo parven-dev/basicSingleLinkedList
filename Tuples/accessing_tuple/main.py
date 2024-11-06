3d5ee8beb1d3b1a7999aa4a516f6956f8d2d125c
ryan <39787691+parven-dev@users.noreply.github.com>
2024-11-06 15:50:46 +0530

    accessing elements

---
+++ Tuples/accessing_tuple/main.py
@@ -0,0 +1,11 @@
+my_tuple = ("a", "b", "c", "d")
+print(type(my_tuple))
+
+accesing_a = my_tuple[0] # same as  in list 
+print(accesing_a)
+
+accesing_last = my_tuple[-1] # as in list
+print(accesing_last)
+
+accessing_specific_element = my_tuple[1:3]
+print(accessing_specific_element) # start from "b" upto "c" , its also start from 0 as in list
