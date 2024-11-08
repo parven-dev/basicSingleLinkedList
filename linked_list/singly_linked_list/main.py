2c8b7b9604487c9fcd606156515a6670efcb1392
ryan <39787691+parven-dev@users.noreply.github.com>
2024-11-08 15:41:12 +0530

    node1 created

--- linked_list/singly_linked_list/main.py
+++ linked_list/singly_linked_list/main.py
@@ -10,14 +10,9 @@ class SlinkedList:
         self.head = None
         self.tail = None
         
-        if self.head is None:
-            node1 = Node(1)
-            node2 = Node(2)
-            self.head = node1
-            self.head.next = node2
-            self.tail = node2
-
-
-
+       
 
 slinked_list = SlinkedList()
+node1 = Node()
+slinked_list.head =node1
+slinked_list.tail = node1
