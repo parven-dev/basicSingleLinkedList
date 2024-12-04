ebcff4e62de1a39a49b0e017b8cbd75c13499e89
ryan <39787691+parven-dev@users.noreply.github.com>
2024-12-04 17:02:00 +0530

    initial commit

---
+++ merge_two_list/main.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next =new_node
+            self.tail = new_node
+            
+        self.length+=1  
+        
+    def display(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+    
+    def merg_sort(self):
+       pass
+   
+                    
+  
+            
+        
+d1 = LinkedList()
+d1.append(40)
+d1.append(70)
+d1.append(60)
+d1.append(50)
+
+# d2 = LinkedList()
+# d2.append(10)
+# d2.append(80)
+# d2.append(30)
+# d2.append(20)
+
+d1.display()
+
+# print(d2.display())
+
+
+# print(d1.display())
